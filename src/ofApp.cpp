e517ebfde61707a68613cec0b8f86bfc143530d9
kimon-satan <simon@simonkatan.co.uk>
2014-10-13 12:51:19 +0100

    added tasks

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,6 +1,10 @@
 #include "ofApp.h"
 
-//TASK - add a new class square particle which inherits particle and overloads the draw method
+//TASK  1a. change the particle's constructor to take a parameter for location
+//      1b. use this to make particles appear when you press the mouse where you press
+
+//TASK  2a. use a conditional statement to cap the number of possible particles
+//      2b. write a loop in update to check the size of particles and remove them when they have a radius of less than 2
 
 const int g_maxNumParticles = 1000; //a global constant variable
 
@@ -10,17 +14,12 @@ void ofApp::setup(){
     ofSetCircleResolution(100);
     ofSetFrameRate(60);
     
-    
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
 
-
-    
     for(int i = 0; i < m_particles.size(); i++)m_particles[i].update();
-    
-    
     
 }
 
@@ -40,7 +39,6 @@ void ofApp::draw(){
     
     ofPopMatrix(); //close the coordinate space
 
-    
     ofSetColor(100);
     
     ofDrawBitmapString("framerate: " + ofToString(ofGetFrameRate()), 20,20);
@@ -66,21 +64,18 @@ void ofApp::mouseMoved(int x, int y){
 
 //--------------------------------------------------------------
 void ofApp::mouseDragged(int x, int y, int button){
-    
-    
+        
 }
 
 //--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button){
     
 
-
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseReleased(int x, int y, int button){
     
-
 
 }
 
